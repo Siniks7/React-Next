f6d7e08376bf576eb58419f2ac0ae4e3068c8b91
siniks7 <siniks7@yandex.ru>
2024-02-01 21:13:11 +0700

    rating

--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,7 @@ export * from './Htag/Htag';
 export * from './Button/Button';
 export * from './P/P';
 export * from './Tag/Tag';
-// export * from './Rating/Rating';
+export * from './Rating/Rating';
 // export * from './Card/Card';
 // export * from './HhData/HhData';
 // export * from './Advantages/Advantages';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import { Button, Htag } from '@/components';
+import { Button, Htag, Rating, Tag } from '@/components';
 import { Noto_Sans } from 'next/font/google';
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const noto_Sans = Noto_Sans({ subsets: ['latin'] });
 
 export default function Home() {
+	const [rating, setRating] = useState<number>(4);
+
 	return (
 		<main
 			className={`flex min-h-screen flex-col items-center justify-between p-24 ${noto_Sans.className}`}
@@ -14,6 +15,11 @@ export default function Home() {
 				<Htag tag='h1'>Текст</Htag>
 				<Button appearance='primary'>Кнопка</Button>
 				<Button arrow='down' appearance='ghost'>Кнопка</Button>
+				<Tag size='s'>Ghost</Tag>
+				<Tag size='m' color='red'>Red</Tag>
+				<Tag size='s' color='green'>Green</Tag>
+				<Tag color='primary'>Green</Tag>
+				<Rating rating={rating} isEditable setRating={setRating} />
 			</>
 		</main>
 	);
