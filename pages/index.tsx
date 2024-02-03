fc99df21aa7e0104f02dc6ae741f7cec88dde800
siniks7 <siniks7@yandex.ru>
2024-02-03 10:41:59 +0700

    ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import { Button, Htag, Rating, Tag } from '@/components';
 import { Noto_Sans } from 'next/font/google';
 import React, { useState } from 'react';
 import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
 
 const noto_Sans = Noto_Sans({ subsets: ['latin'] });
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
@@ -21,9 +24,30 @@ function Home(): JSX.Element {
 				<Tag size='s' color='green'>Green</Tag>
 				<Tag color='primary'>Green</Tag>
 				<Rating rating={rating} isEditable setRating={setRating} />
+				<ul>
+					{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+				</ul>
 			</>
 		</main>
 	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
