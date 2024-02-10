a1ac07d681d1c420e14b44ef97b28e4320971b40
siniks7 <siniks7@yandex.ru>
2024-02-10 17:38:00 +0700

    dsh

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,4 +1,4 @@
-import { HhData, Htag, Sort, Tag } from '@/components';
+import { HhData, Htag, Product, Sort, Tag } from '@/components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import { TopLevelCategory } from '@/interfaces/page.interface';
 import styles from './TopPageComponent.module.css';
@@ -24,7 +24,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 				<Sort sort={sort} setSort={setSort} />
 			</div>
 			<div>
-				{sortedProducts && sortedProducts.map(p => (<div key={p._id}>{p.title}</div>))}
+				{sortedProducts && sortedProducts.map(p => (<Product key={p._id} product={p} />))}
 			</div>
 			<div className={styles.hhTitle}>
 				<Htag tag='h2'>Вакансии - {page.category}</Htag>
