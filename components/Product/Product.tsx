d790a8af50f843001e847bcde866a955eb12ec9e
siniks7 <siniks7@yandex.ru>
2024-02-11 12:31:14 +0700

    agag

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -12,7 +12,7 @@ import Image from 'next/image';
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product}>
-			<div className={styles.logo}><Image src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
+			<div className={styles.logo}><Image width={70} height={70} src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
 			<div className={styles.title}>{product.title}</div>
 			<div className={styles.price}>
 				{priceRu(product.price)}
