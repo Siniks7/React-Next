045d2b6a3308138300da486707809a725f7aaa81
siniks7 <siniks7@yandex.ru>
2024-02-11 21:13:51 +0700

    436

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { priceRu } from '@/helpers/helpers';
+import { declOfNum, priceRu } from '@/helpers/helpers';
 import { ProductProps } from './Product.props';
 import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
@@ -8,6 +8,7 @@ import { Button } from '../Button/Button';
 import { Divider } from '../Divider/Divider';
 import styles from './Product.module.css';
 import Image from 'next/image';
+import cn from 'classnames';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
@@ -25,10 +26,18 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 			<div className={styles.tags}>{product.categories.map(c => <Tag key={c} className={styles.category} color='ghost'>{c}</Tag>)}</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
-			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
-			<Divider className={styles.hr} />
+			<div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
+			<Divider className={cn(styles.hr, styles.hr2)} />
 			<div className={styles.description}>{product.description}</div>
-			<div className={styles.feature}>фичи</div>
+			<div className={styles.feature}>
+				{product.characteristics.map(c => (
+					<div className={styles.characteristics} key={c.name}>
+						<span className={styles.characteristicsName}>{c.name}</span>
+						<span className={styles.characteristicsDots}></span>
+						<span className={styles.characteristicsValue}>{c.value}</span>
+					</div>
+				))}
+			</div>
 			<div className={styles.advBlock}>
 				{product.advantages && <div className={styles.advantages}>
 					<div className={styles.advTitle}>Преимущества</div>
--- components/index.ts
+++ components/index.ts
@@ -11,7 +11,7 @@ export * from './Input/Input';
 export * from './Textarea/Textarea';
 export * from './Search/Search';
 export * from './Product/Product';
-// export * from './Divider/Divider';
+export * from './Divider/Divider';
 // export * from './Review/Review';
 // export * from './ReviewForm/ReviewForm';
 // export * from './Up/Up';
