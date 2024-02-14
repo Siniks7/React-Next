ef9549bfb3877749cc8c3b8ced442e052b383ee1
siniks7 <siniks7@yandex.ru>
2024-02-14 23:48:45 +0700

    25

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -11,7 +11,7 @@ import { IReviewForm } from './ReviewForm.interface';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
-	const { register, control, handleSubmit } = useForm<IReviewForm>();
+	const { register, control, handleSubmit,  formState: { errors } } = useForm<IReviewForm>();
 
 	const onSubmit = (data: IReviewForm) => {
 		console.log(data);
@@ -22,19 +22,33 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
 			<div className={cn(styles.reviewForm, className)}
 				{...props}
 			>
-				<Input {...register('name')} placeholder='Имя' />
-				<Input {...register('title')} placeholder='Заголовок отзыва' className={styles.title} />
+				<Input
+					{...register('name', { required: { value: true, message: 'Заполните имя' } })}
+					placeholder='Имя'
+					error={errors.name}
+				/>
+				<Input
+					{...register('title', { required: { value: true, message: 'Заполните заголовок' } })}
+					placeholder='Заголовок отзыва'
+					className={styles.title}
+					error={errors.title}
+				/>
 				<div className={styles.rating}>
 					<span>Оценка:</span>
 					<Controller
 						control={control}
 						name='rating'
 						render={({ field }) => (
-							<Rating isEditable rating={field.value} setRating={field.onChange} />
+							<Rating isEditable rating={field.value} ref={field.ref}  setRating={field.onChange} />
 						)}
 					/>
 				</div>
-				<Textarea  {...register('description')} placeholder='Текст отзыва' className={styles.description} />
+				<Textarea
+					{...register('description', { required: { value: true, message: 'Заполните описание' } })}
+					placeholder='Текст отзыва'
+					className={styles.description}
+					error={errors.description}
+				/>
 				<div className={styles.submit}>
 					<Button appearance="primary">Отправить</Button>
 					<span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -1,9 +1,17 @@
 import { TextareaProps } from './Textarea.props';
 import styles from './Textarea.module.css';
 import cn from 'classnames';
+import { ForwardedRef, forwardRef } from 'react';
 
-export const Textarea = ({ className, ...props }: TextareaProps): JSX.Element => {
+
+// eslint-disable-next-line react/display-name
+export const Textarea = forwardRef(({ className, error, ...props }: TextareaProps, ref: ForwardedRef<HTMLTextAreaElement>): JSX.Element => {
 	return (
-		<textarea className={cn(className, styles.input)} {...props} />
+		<div className={cn(className, styles.inputWrapper)}>
+			<textarea className={cn(styles.input, {
+				[styles.error]: error
+			})} ref={ref} {...props} />
+			{error && <span role="alert" className={styles.errorMessage}>{error.message}</span>}
+		</div>
 	);
-};
+});
